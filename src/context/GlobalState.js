a7088a66e106fdd55276f251320a4dc0c0275943
Mottahir <mottahir@users.noreply.github.com>
2020-06-19 22:10:01 +0300

    GlobbalState Update
     Dispatch was not used for i removed it

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -20,7 +20,8 @@ export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = ({children}) => {
 
-    const [state, dispatch] = useReducer(AppReducer, initialState)
+    // const [state, dispatch] = useReducer(AppReducer, initialState)
+   const [state] = useReducer(AppReducer, initialState)
 
     return(
         <GlobalContext.Provider value ={
